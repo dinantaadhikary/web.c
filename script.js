7acaf84449498d7ac7c0dbfaca0f41f314f1f400
dinantaadhikary <156471095+dinantaadhikary@users.noreply.github.com>
2024-01-15 21:52:30 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+function white()
+        {
+            document.body.style.backgroundColor = "white";
+            let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-0');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-0');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-0');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList=[]
+    elem4.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0",'nw');
+    let elem5 = document.getElementById('b1');
+    elem5.classList=[]
+    elem5.classList.add('nw',"btn","btn-j")
+    let elem6=document.getElementById('a1');
+    elem6.classList=[]
+    elem6.classList.add('nw',"btn","btn-1")
+    let elem7=document.getElementById('a2');
+    elem7.classList=[]
+    elem7.classList.add("btn","btn-2")
+    let elem8=document.getElementById('up');
+    elem8.classList=[]
+    elem8.classList.add("learn")
+    let elem9=document.getElementById('pd');
+    elem9.classList=[]
+    elem9.classList.add("dive")
+    let elem10=document.getElementById('jk');
+    elem10.classList=[]
+    elem10.classList.add("me-2","src-bg");
+ }
+
+        function purple()
+        {
+            document.body.style.backgroundColor = "#7049C3";
+            let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-1');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-1');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-1');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList=[]
+    elem4.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0",'np');
+    let elem5 = document.getElementById('b1');
+    elem5.classList=[]
+    elem5.classList.add('np',"btn","btn-j")
+    let elem6=document.getElementById('a1');
+    elem6.classList=[]
+    elem6.classList.add('np',"btn","btn-1")
+    let elem7=document.getElementById('a2');
+    elem7.classList=[]
+    elem7.classList.add('dd',"btn","btn-2")
+    let elem8=document.getElementById('up');
+    elem8.classList=[]
+    elem8.classList.add("opp","learn")
+    let elem9=document.getElementById('pd');
+    elem9.classList=[]
+    elem9.classList.add("opp","dive")
+    let elem10=document.getElementById('jk');
+    elem10.classList=[]
+    elem10.classList.add("ao","me-2","src-bg");
+ }
+        function blue()
+        { 
+            document.body.style.backgroundColor = "#0A2B9F";
+            let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-2');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-2');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-2');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList=[]
+    elem4.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0",'nb');
+    let elem5 = document.getElementById('b1');
+    elem5.classList=[]
+    elem5.classList.add('nb',"btn","btn-j")
+    let elem6=document.getElementById('a1');
+    elem6.classList=[]
+    elem6.classList.add('nb',"btn","btn-1")
+    let elem7=document.getElementById('a2');
+    elem7.classList=[]
+    elem7.classList.add('dd',"btn","btn-2")
+    let elem8=document.getElementById('up');
+    elem8.classList=[]
+    elem8.classList.add("opp","learn")
+    let elem9=document.getElementById('pd');
+    elem9.classList=[]
+    elem9.classList.add("opp","dive")
+    let elem10=document.getElementById('jk');
+    elem10.classList=[]
+    elem10.classList.add("ao","me-2", "src-bg");
+
+        }
+        function red()
+        {
+            document.body.style.backgroundColor = "#B54242";
+            let elem1 = document.getElementById('h');
+    elem1.classList=[]
+    elem1.classList.add('hs-3');
+    let elem2 = document.getElementById('g');
+    elem2.classList=[]
+    elem2.classList.add('hs-3');
+    let elem3 = document.getElementById('c');
+    elem3.classList=[]
+    elem3.classList.add('hs-3');
+    let elem4 = document.getElementById('nav1');
+    elem4.classList=[]
+    elem4.classList.add("navbar-nav", "me-auto", "mb-2", "mb-lg-0",'nr');
+    let elem5 = document.getElementById('b1');
+    elem5.classList=[]
+    elem5.classList.add('nr',"btn","btn-j")
+    let elem6=document.getElementById('a1');
+    elem6.classList=[]
+    elem6.classList.add('nr',"btn","btn-1")
+    let elem7=document.getElementById('a2');
+    elem7.classList=[]
+    elem7.classList.add('dd',"btn","btn-2")
+    let elem8=document.getElementById('up');
+    elem8.classList=[]
+    elem8.classList.add("opp","learn")
+    let elem9=document.getElementById('pd');
+    elem9.classList=[]
+    elem9.classList.add("opp","dive")
+    let elem10=document.getElementById('jk');
+    elem10.classList=[]
+    elem10.classList.add("ao","me-2", "src-bg");
+        }
+        
